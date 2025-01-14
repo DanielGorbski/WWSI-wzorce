67a2cc3becbcd19b2867fbaf552bdb63659cd580
DanielGorbski <danielgor@onet.pl>
2025-01-14 23:16:19 +0100

    Add files via upload

---
+++ Odrzuconyspadek.cs
@@ -0,0 +1,62 @@
+﻿
+public interface IEmployee
+{
+    void Work();
+    void AttendMeeting();
+}
+
+
+public class Employee : IEmployee
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+
+    public void Work()
+    {
+        /* Implementacja */
+    }
+
+    public void AttendMeeting()
+    {
+        /* Implementacja */
+    }
+}
+
+
+public class Manager : IEmployee
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+
+  
+    public void Work()
+    {
+        /* Implementacja dla managera */
+    }
+
+    public void AttendMeeting()
+    {
+        /* Implementacja dla managera */
+    }
+
+    public void ManageTeam()
+    {
+        /* Implementacja */
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IEmployee employee = new Employee();
+        employee.Work();
+        employee.AttendMeeting();
+
+        Manager manager = new Manager();
+        manager.Work();
+        manager.AttendMeeting();
+        manager.ManageTeam();
+    }
+}
