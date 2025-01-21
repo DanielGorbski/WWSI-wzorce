6ea5992f7c48798d2badee2a3fd437c7082c48b9
DanielGorbski <danielgor@onet.pl>
2025-01-21 20:23:27 +0100

    2

---
+++ Adapter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Adult
+{
+    private int age;
+
+    public Adult(int age)
+    {
+        this.age = age;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+}
+
+public class Validator
+{
+    public bool IsAdult(Adult person)
+    {
+        return person.GetAge() >= 18;
+    }
+}
+
+
+public class FakeAdult : Adult
+{
+    private int realAge;
+
+    public FakeAdult(int realAge) : base(21)
+    {
+        this.realAge = realAge;
+    }
+
+
+    public int GetRealAge()
+    {
+        return realAge;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Validator validator = new Validator();
+
+
+        FakeAdult krzys = new FakeAdult(17);
+
+        Console.WriteLine("Czy przeszedł walidację: " + validator.IsAdult(krzys));
+        Console.WriteLine("Rzeczywisty wiek: " + krzys.GetRealAge());
+    }
+}
