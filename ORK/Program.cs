23fea6801df7051a808c93a1f805fb8a3cbb962e
DanielGorbski <danielgor@onet.pl>
2025-01-21 20:14:50 +0100

    2

---
+++ ORK/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PrototypOrka
+{
+    public class Ork
+    {
+        public string Imie
+        {
+            get;
+            set;
+        }
+        public int Sila
+        {
+            get;
+            set;
+        }
+        public int Zdrowie
+        {
+            get;
+            set;
+        }
+        public string Bron
+        {
+            get;
+            set;
+        }
+        public DateTime DataUtworzenia
+        {
+            get;
+            set;
+        }
+
+        public Ork()
+        {
+            Random losowanie = new Random();
+            Sila = losowanie.Next(10, 201);
+            Zdrowie = losowanie.Next(50, 301);
+            DataUtworzenia = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"Ork: {Imie}, Siła: {Sila}, Zdrowie: {Zdrowie}, Broń: {Bron}, Data: {DataUtworzenia}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Ork ork = new Ork
+            {
+                Imie = "Grom",
+                Bron = "Topor"
+            };
+
+            var serializator = new Newtonsoft.Json.JsonSerializer();
+            serializator.Converters.Add(new JavaScriptDateTimeConverter());
+            serializator.NullValueHandling = NullValueHandling.Ignore;
+
+            List<Ork> armiaOrkow = new List<Ork>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                using (StreamWriter sw = new StreamWriter(@"c:\temp\ork.txt"))
+                using (Newtonsoft.Json.JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializator.Serialize(writer, ork);
+                }
+
+                using (StreamReader sr = new StreamReader(@"c:\temp\ork.txt"))
+                using (Newtonsoft.Json.JsonReader reader = new JsonTextReader(sr))
+                {
+                    Ork klon = serializator.Deserialize<Ork>(reader);
+                    klon.Imie = $"Klon Groma {i + 1}";
+                    // Nowe statystyki są generowane przez konstruktor
+                    armiaOrkow.Add(klon);
+                }
+            }
+
+            Console.WriteLine("Oryginalny Ork:");
+            Console.WriteLine(ork);
+            Console.WriteLine("\nSklonowane Orki:");
+            foreach (var klon in armiaOrkow)
+            {
+                Console.WriteLine(klon);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+public interface IStreamingService
+{
+    void PlayContent();
+}
+
+public class BasicSubscription : IStreamingService
+{
+    public void PlayContent()
+    {
+        Console.WriteLine("\nOdtwarzanie zawartości z pakietu podstawowego:");
+        Console.WriteLine("- Filmy w jakości HD");
+        Console.WriteLine("- Jeden profil użytkownika");
+        Console.WriteLine("- Reklamy podczas odtwarzania");
+    }
+}
+
+public class PremiumSubscription : IStreamingService
+{
+    public void PlayContent()
+    {
+        Console.WriteLine("\nOdtwarzanie zawartości z pakietu premium:");
+        Console.WriteLine("- Filmy w jakości 4K HDR");
+        Console.WriteLine("- Pięć profili użytkownika");
+        Console.WriteLine("- Brak reklam");
+        Console.WriteLine("- Dodatkowe materiały zza kulis");
+    }
+}
+
+public class PremiumSubscriptionProxy : IStreamingService
+{
+    private readonly PremiumSubscription premiumSubscription;
+    private const string VALID_PASSWORD = "premium123";
+
+    public PremiumSubscriptionProxy()
+    {
+        premiumSubscription = new PremiumSubscription();
+    }
+
+    public void PlayContent()
+    {
+        Console.Write("\nPodaj hasło do subskrypcji premium: ");
+        string enteredPassword = ReadPassword();
+
+        if (enteredPassword == VALID_PASSWORD)
+        {
+            premiumSubscription.PlayContent();
+        }
+        else
+        {
+            Console.WriteLine("\nNieprawidłowe hasło! Dostęp zabroniony.");
+        }
+    }
+
+    private string ReadPassword()
+    {
+        string password = "";
+        ConsoleKeyInfo key;
+
+        do
+        {
+            key = Console.ReadKey(true);
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                if (key.Key != ConsoleKey.Backspace)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Substring(0, password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+        }
+        while (key.Key != ConsoleKey.Enter);
+
+        Console.WriteLine();
+        return password;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n=== Serwis Streamingowy ===");
+            Console.WriteLine("1. Odtwórz zawartość z pakietu podstawowego");
+            Console.WriteLine("2. Odtwórz zawartość z pakietu premium");
+            Console.WriteLine("3. Wyjście");
+
+            Console.Write("\nWybierz opcję (1-3): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    IStreamingService basicSubscription = new BasicSubscription();
+                    basicSubscription.PlayContent();
+                    break;
+
+                case "2":
+                    IStreamingService premiumProxy = new PremiumSubscriptionProxy();
+                    premiumProxy.PlayContent();
+                    break;
+
+                case "3":
+                    Console.WriteLine("\nDziękujemy za korzystanie z serwisu!");
+                    return;
+
+                default:
+                    Console.WriteLine("\nNieprawidłowa opcja. Spróbuj ponownie.");
+                    break;
+            }
+        }
+    }
+}
